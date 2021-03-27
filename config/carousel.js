e9715886ac85bbf8ee75e3c57863634c6f3b340e
Filipe Cancellier da Costa <filipecancelliercosta@gmail.com>
2021-03-27 10:58:01 +0000

    created carrousel for welcome

---
+++ config/carousel.js
@@ -0,0 +1,35 @@
+const slidesImages = document.querySelectorAll('.slidesImg')
+const carouselContainer = document.querySelector('.carouselSlider')
+
+let timeInterval;
+const interval = 5000
+
+function ImageSlider(orientation) {
+  const imgSelected = document.querySelector('.selected')
+  imgSelected.classList.remove('selected')
+  
+  clearInterval(timeInterval)
+
+  if (orientation === 'next') {
+    if (imgSelected.nextElementSibling) {
+      imgSelected.nextElementSibling.classList.add('selected')
+    } else {
+      slidesImages[0].classList.add('selected')
+    }
+  } else {
+    if (imgSelected.previousElementSibling) {
+      imgSelected.previousElementSibling.classList.add('selected')
+    } else {
+      slidesImages[slidesImages.length - 1].classList.add('selected')
+    }
+  }
+
+  carouselLoop()
+};
+
+function carouselLoop() {
+  timeInterval = setInterval(() => ImageSlider('next'), interval)
+}
+
+
+carouselLoop()
